4ac7f46382d5cdaa7ce396787f13661beecc09d6
miaoz <miaoz@live.cn>
2026-08-21 14:59:24 +0800

    Bump version to 1.10.4 (43)

--- scripts/package_app.sh
+++ scripts/package_app.sh
@@ -8,8 +8,8 @@ BUNDLE_ID="com.miaoz.wick"
 MIN_SYSTEM_VERSION="13.0"
 # Optional overrides for CI / tagged releases:
 #   VERSION=1.4.0 BUILD=42 ./scripts/package_app.sh
-VERSION="${VERSION:-1.10.3}"
-BUILD="${BUILD:-42}"
+VERSION="${VERSION:-1.10.4}"
+BUILD="${BUILD:-43}"
 DIST_DIR="$ROOT_DIR/dist"
 APP_DIR="$DIST_DIR/$APP_NAME.app"
 LEGACY_APP_DIR="$DIST_DIR/$LEGACY_APP_NAME.app"
