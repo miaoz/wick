e10c68cbca22bddf00ef403355d2af981e528f5b
miaoz <miaoz@live.cn>
2026-08-24 15:18:57 +0800

    feat(phone): enable native iOS screen edge swipe-to-back gesture across all secondary views

---
+++ ios/WickPhone/SwipeBackEnabler.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+#if os(iOS)
+extension UINavigationController: @retroactive UIGestureRecognizerDelegate {
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+        interactivePopGestureRecognizer?.delegate = self
+    }
+
+    public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        return viewControllers.count > 1
+    }
+
+    public func gestureRecognizer(
+        _ gestureRecognizer: UIGestureRecognizer,
+        shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer
+    ) -> Bool {
+        return true
+    }
+}
+#endif
